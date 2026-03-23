e2ab82823848d2c403d7a989669b094370ab4cc5
Nikodemus <nikodembilinski4@gmail.com>
2026-03-23 19:23:02 +0100

    added creating new tags

--- Pages/TagPage.xaml.cs
+++ Pages/TagPage.xaml.cs
@@ -40,4 +40,45 @@ public partial class TagPage : ContentPage
 	{
 		await Load_Tags();
 	}
+
+    private async void Add_Tag(object sender, EventArgs e)
+    {
+		string name = await DisplayPromptAsync("New Tag", "Enter the name of the new tag:", "OK", "Cancel");
+
+		if (string.IsNullOrEmpty(name))
+		{
+			return;
+		}
+
+		string colorName = await DisplayActionSheetAsync("Pick a color", "Cancel", null, "Purple", "Blue", "Lime","Green", "Yellow", 
+			"Pink", "Orange", "Gold", "Maroon", "Royal", "Violet");
+
+        string hex = colorName switch
+        {
+            "Purple" => "#800080",
+            "Blue" => "#0000FF",
+            "Lime" => "#00FF00",
+            "Green" => "#008000",
+            "Yellow" => "#FFFF00",
+            "Pink" => "#FFC0CB",
+            "Orange" => "#FFA500",
+            "Gold" => "#FFD700",
+            "Maroon" => "#800000",
+            "Royal" => "#4169E1",
+            "Violet" => "#EE82EE",
+            _ => "#FFFFFF" // Default white color if anything happens
+        };
+
+		Tag newtag = new Tag
+		{
+			Name = name,
+			ColorHex = hex,
+			IsActive = false
+
+		};
+
+		await _databaseservice.AddTag(newtag);
+
+		await Refresh_Tags();
+    }
 }
