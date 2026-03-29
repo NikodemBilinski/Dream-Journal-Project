22c2bbaea873829f17ddc48d286ceea1fba36ba2
Nikodemus <nikodembilinski4@gmail.com>
2026-03-29 13:04:12 +0200

    cleaned up chartpage.xaml.cs

--- Pages/ChartPage.xaml.cs
+++ Pages/ChartPage.xaml.cs
@@ -30,70 +30,11 @@ public partial class ChartPage : ContentPage
     {
         base.OnAppearing();
 
-        try
-        {
-            await GenerateChart2();
-
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine($"Error generating chart: "+ ex.Message);
-        }
-
         var taglist = await _dataBaseService.GetTags();
 
         ExpanderList.ItemsSource = taglist;
 
 
-    }
-
-
-    // for now it will stay commented until i make it work with tags
-    public async Task GenerateChart2()
-    {
-        //// get dreams from database to collection
-        //var dreamsFromDb = await _dataBaseService.GetDreams();
-        //Debug.WriteLine($"sny:  {dreamsFromDb.Count}");
-        //Dreams.Clear();
-        //foreach (var dream in dreamsFromDb)
-        //{
-        //    Dreams.Add(dream);
-        //}
-
-        //// get number of specific dreams to ints
-        ////int lucidCount = Dreams.Count(x => x.LucidDream);
-        ////int nonlucidCount = Dreams.Count(x => !x.LucidDream);
-        ////int dreamsTotal = Dreams.Count();
-
-        //// create entries for chart (Lucid and non lucid dreams)
-        //var entries = new[]
-        //{
-        //    new ChartEntry(lucidCount)
-        //    {
-        //        Color = SKColors.Red
-        //    },
-
-        //    new ChartEntry(nonlucidCount)
-        //    {
-        //        Color = SKColors.Green
-                
-        //    }
-
-        //};
-
-        //MyChart = new DonutChart
-        //{
-        //    Entries = entries,
-        //    BackgroundColor = SKColor.Empty,
-        //    LabelTextSize = 15,
-        //    Typeface = SKTypeface.FromFamilyName("Arial"),
-        //    LabelColor = SKColors.White
-        //};
-
-        //DreamChart.Chart = MyChart;
-
-        //ChartLucidLabel.Text = "Lucid Count: " + lucidCount.ToString();
-        //ChartNonLucidLabel.Text = "Non-Lucid Count:  " + nonlucidCount.ToString();
     }
 
     
@@ -103,8 +44,19 @@ public partial class ChartPage : ContentPage
 
         if (tag != null)
         {
+
             await RemoveChart();
-            await GenerateChart(tag);
+            try
+            {
+                await GenerateChart(tag);
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error generating chart: " + ex.Message);
+            }
+           
+            
             
         }
         
@@ -115,6 +67,8 @@ public partial class ChartPage : ContentPage
     {
         DreamChart.Chart = null;
     }
+
+
     private async Task GenerateChart(Tag tag)
     {
         List<Dream> AllDreams = await _dataBaseService.GetDreams();
