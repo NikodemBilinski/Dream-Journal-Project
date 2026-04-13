3b6957179d948e0a1dec413184b7a73fbb66574f
Nikodemus <nikodembilinski4@gmail.com>
2026-04-13 13:21:15 +0200

    started working on about page

--- Pages/AboutAppPage.xaml.cs
+++ Pages/AboutAppPage.xaml.cs
@@ -6,4 +6,30 @@ public partial class AboutAppPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+
+        var currentTheme = Preferences.Default.Get("AppTheme", AppTheme.Unspecified.ToString());
+
+        if (Enum.TryParse(currentTheme, out AppTheme theme))
+        {
+            Application.Current.UserAppTheme = theme;
+        }
+
+
+        if (theme == AppTheme.Light)
+        {
+            GithubImage.Source = "github_light.png";
+            GithubBorder.BackgroundColor = Colors.Black;
+        }
+        else if(theme == AppTheme.Dark)
+        {
+            GithubImage.Source = "github_betterdark.png";
+            GithubImage.CornerRadius = 50;
+            GithubBorder.BackgroundColor = Colors.White;
+        }
+    }
 }
